d238fa9fca6bc2d9fd17da9320cae86f48df1007
ez720 <elenazheng72@gmail.com>
2019-11-20 23:39:20 -0600

    Added documentation

--- fantastic-finale-ez720/src/gameUtil/board.hpp
+++ fantastic-finale-ez720/src/gameUtil/board.hpp
@@ -28,13 +28,48 @@ private:
     vector<char> board_state;
     
 public:
+    /*
+     Constructor for board
+     Creates a board_length * board_length vector to represent a board
+     */
     Board(int board_length);
+    
+    /*
+     Sets the current board to the starting board for Othello
+     Each player has two pieces that are diagonal to each other in the center of the board
+     */
     void SetInitialBoard();
+    
+    /*
+     Pretty printing for the board
+     Black is "X", White is "O", Empty is "."
+     */
     void PrintBoard();
+    
+    /*
+     Returns an index of a tile where the player flanks all opponent tiles in the direction
+     If the player can't flank the opponent in the direction, return -1
+     */
     int GetFlankIndex(int start_index, Player player, int direction);
+    
+    /*
+     
+     */
     void FlipTiles(int start_index, Player player, int direction);
+    
+    /*
+     Check if player can flank the opponent from any direction
+     */
     bool IsValidMove(int start_index, Player player);
+    
+    /*
+     
+     */
     void MakeMove(int start_index, Player player);
+    
+    /*
+     Returns a vector of valid indices the player can move
+     */
     vector<int> GetValidMoves(Player player);
 };
 
