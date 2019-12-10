add8ffce14d365941317f12955c588d45ee85f87
ez720 <elenazheng72@gmail.com>
2019-12-10 09:13:33 -0600

    Removed unused files

--- fantastic-finale-ez720/New Group/tests-main.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  tests-main.cpp
-//  fantastic-finale-ez720-tests
-//
-//  Created by Elena Zheng on 11/22/19.
-//
-#define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_COMPILE_FAST
-#include "catch.hpp"
